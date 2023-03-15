69c204501ab49d303a39e8490fbdfeb1e4344c75
clgtmm <103909534+clgtmm@users.noreply.github.com>
2023-03-15 14:30:56 +0700

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,1 +1,8 @@
-
+getgenv().setting = {
+    Fov = 300,
+    Color = Color3.fromRGB(191, 255, 209),
+    LockPlayers = false,
+    LockPlayersBind = Enum.KeyCode.L,
+    resetPlayersBind = Enum.KeyCode.P,
+}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/clgtmm/silent-aim/main/silent.lua'))()
